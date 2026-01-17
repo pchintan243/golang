4fe7456ab3b1197bfd19457db9fb9fd41c308c98
Chintan Patel <chintan.patel@artemhealthtech.com>
2026-01-18 00:14:44 +0530

    By Mistake Removed

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,1 +1,15 @@
+package types
 
+type Student struct {
+	Id    int64  `json:"id"`
+	Name  string `json:"name" validate:"required"`
+	Email string `json:"email" validate:"required"`
+	Age   int    `json:"age" validate:"required"`
+}
+
+type UpdateStudentRequest struct {
+	Id    int64  `json:"id" validate:"required"`
+	Name  string `json:"name" validate:"required"`
+	Email string `json:"email" validate:"required,email"`
+	Age   int    `json:"age" validate:"required,gte=0"`
+}
